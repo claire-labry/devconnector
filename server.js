7dae980d94bfd60cf907e1ad1d38b3fed110f5eb
claire-labry <clairestjlabry@gmail.com>
2020-06-11 10:40:11 -0500

    pushing the remaining file

--- server.js
+++ server.js
@@ -22,3 +22,5 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// line of code
