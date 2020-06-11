73a7867842f13eca3bc514e81f4f60d9840620ff
claire-labry <clairestjlabry@gmail.com>
2020-06-11 10:37:02 -0500

    adding files

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-// @route   GET api/profile/me
-// @desc    Get current user's profile
-// @access  Private
 router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({
@@ -23,5 +21,63 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+
+    console.log(skills);
+
+    res.send('Hello');
+    // if (youtube) profileFields.youtube = youtube;
+    // if (facebook) profileFields.facebook = facebook;
+    // if (twitter) profileFields.twitter = twitter;
+    // if (instagram) profileFields.instagram = instagram;
+    // if (linkedin) profileFields.linkedin = linkedin;
+  }
+);
 
 module.exports = router;
