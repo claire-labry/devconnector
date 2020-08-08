9c2b1cf9ed74e0fca05399dcb9b850ba5d7812a4
claire-labry <clairestjlabry@gmail.com>
2020-08-08 16:40:53 -0500

    pausing before password encryption;

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const { body, validationResult } = require('express-validator')
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route    POST api/users
 // @desc     Register user
@@ -12,16 +15,46 @@ router.post(
     body('email', 'Please include a valid email').isEmail(),
     body(
       'password',
-      'Please enter a password with 6 or more characters'
-    ).isLength({ min: 6 })
+      'Please enter a password with 6 or more characters',
+    ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json( {errors: errors.array()} );
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route')
-  }
-)
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+          s:'200',
+          r: 'pg',
+          d: 'mm'
+      })
+
+      user = new User({
+          name, 
+          email,
+          avatar,
+          password
+      });
+
+      // encrypt password
+
+      // return jsonwebtoken
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  },
+);
 
 module.exports = router;
